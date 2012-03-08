264e29f907a75a14c1b21b0ecdbfd5dc4c6c533a
holgerl <holger.ludvigsen@bekk.no>
2012-03-08 17:36:51 +0100

    Refaktorering av slidifier.js

--- js/slidifier.js
+++ js/slidifier.js
@@ -1,53 +1,52 @@
 var slides;
 var slideCounter;
 
+var submitHandler = function(event) {
+	event.preventDefault();
+	
+	var slidesSrc = $('textarea[name=slides_src]').val();
+	slides = processSlideSource(slidesSrc);
+	
+	if (isIE()) {
+		for (var i in slides) {
+			slides[i] = slides[i].replace(/\n/g, "<br/>");
+		}
+	}
+	
+	slideCounter = 0;
+	
+	refresh();
+	
+	loadTheme();
+	
+	showSlideShow();
+};
+
+var showSlideShow = function() {
+	$("#slideEdit").fadeOut(10, function() {
+		$("#slide").fadeIn(1000, function() {
+			$("#help").slideDown("slow", function() {
+				setTimeout(function() {
+					$("#help").slideUp("slow");
+				}, 3000);
+			});
+		});
+	});
+};
+
+var isIE = function() {
+	return navigator.userAgent.indexOf('MSIE') != -1;
+};
+
 $(document).ready(function() {
 	$("#slide").hide();
+	$("#urls").hide();
+	$("#help").hide();
 	
 	$('#srcForm textarea').TextAreaResizer();
 	
 	updateClock();
 	setInterval('updateClock()', 1000*60);
 	
-	// for debugging only:
-	slides = $('textarea[name=slides_src]').val();
-	slides = processSlideSource(slides);
-	// end 
-	
-	$("#srcForm").submit(function() {
-		slides = $('textarea[name=slides_src]').val();
-		slides = processSlideSource(slides);
-		
-		if (isIE()) {
-			for (var i in slides) {
-				slides[i] = slides[i].replace(/\n/g, "<br/>");
-			}
-		}
-		
-		slideCounter = 0;
-		
-		refresh();
-		
-		loadTheme();
-		
-		$("#slideEdit").fadeOut(10, function() {
-			$("#slide").fadeIn(1000, function() {
-				$("#help").slideDown("slow", function() {
-					setTimeout(function() {
-						$("#help").slideUp("slow");
-					}, 3000);
-				});
-			});
-		});
-		
-		return false;
-	});
-
+	$("#srcForm").submit(submitHandler);
 });
-
-function isIE() {
-	if (navigator.userAgent.indexOf('MSIE') !=-1) {
-		return true;
-	}
-	return false;
-}
