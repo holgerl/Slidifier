04ab45588bd3b69c76bb6435b2757b5572cd9af7
holgerl <holger.ludvigsen@bekk.no>
2012-03-08 18:27:08 +0100

    Putter slideparser.js i eget scope

--- js/slideparser.js
+++ js/slideparser.js
@@ -1,139 +1,143 @@
-var processSlideSource = function(slideSrc) {
-	var lines = cbSplit(slideSrc, /\n/);
-	var tokens = new Array();
-	
-	for (var i in lines) {
-		var token = tokenize(lines[i]);
-		tokens.push(token);
-	}
-	
-	tokens = removeEmptyLinesAroundSlidedelimiters(tokens);
-	
-	tokens = applyDynamics(tokens);
-	
-	var slide = "";
-	slides = new Array();
-	
-	var inCode = false;
-	var inList = false;
-	for (var i in tokens) {
+var slideparser = (function() {
+	this.processSlideSource = function(slideSrc) {
+		var lines = cbSplit(slideSrc, /\n/);
+		var tokens = new Array();
+		
+		for (var i in lines) {
+			var token = tokenize(lines[i]);
+			tokens.push(token);
+		}
+		
+		tokens = removeEmptyLinesAroundSlidedelimiters(tokens);
+		
+		tokens = applyDynamics(tokens);
+		
+		var slide = "";
+		slides = new Array();
+		
+		var inCode = false;
+		var inList = false;
+		for (var i in tokens) {
+			if (inCode) {
+				if (tokens[i].token != "codemark") {
+					slide += escapeTags(tokens[i].line+"\n");
+				} else {
+					slide += "</pre>";
+					inCode = false;
+				}
+				continue;
+			}
+			
+			if (inList) {
+				if (tokens[i].token != "bullet") {
+					slide += "</ul>";
+					inList = false;
+				}
+			}
+			
+			tokens[i].body = unescapeWhitelist(escapeTags(tokens[i].body));
+			
+			switch (tokens[i].token) {
+				case "slidemark":
+					slides.push(slide);
+					slide = "";
+					break;
+				case "bullet":
+					if (!inList) slide += "<ul>";
+					slide += "<li class='lvl"+countNumberOf("-",tokens[i].keyword)+"'>"+tokens[i].body+"</li>";
+					inList = true;
+					break;
+				case "heading":
+					var lvl = countNumberOf("#",tokens[i].keyword);
+					var clazz = tokens[i].addClass != undefined ? " class='"+tokens[i].addClass+"'" : "";
+					slide += "<h"+lvl+clazz+">"+tokens[i].body+"</h"+lvl+">";
+					break;
+				case "codemark":
+					slide += "<pre class='prettyprint'>\n";
+					inCode = true;
+					break;
+				case "empty":
+					slide += tokens[i].body+"\n";
+					break;
+				case "catchall":
+					slide += tokens[i].body+"\n";
+					break;
+			}
+		}
+		
 		if (inCode) {
-			if (tokens[i].token != "codemark") {
-				slide += escapeTags(tokens[i].line+"\n");
-			} else {
-				slide += "</pre>";
-				inCode = false;
-			}
-			continue;
+			slide += "</pre>\n";
 		}
-		
 		if (inList) {
-			if (tokens[i].token != "bullet") {
-				slide += "</ul>";
-				inList = false;
+			slide += "</ul>\n";
+		}
+		
+		slides.push(slide);
+		
+		slides.push(endOfShowSlide());
+		
+		return slides;
+	}
+	
+	var tokenize = function(line) {
+		var regexes = [
+			["slidemark", 	/^\s*(-\s*-\s*-)\s*()$/, 			1, 2],
+			["bullet", 		/^\s*((-\s*)*-)\s*(([^-]|\S)+)$/, 	1, 3],
+			["heading", 	/^\s*((#\s*)*#)\s*(([^#]|\S)+)$/, 	1, 3],
+			["codemark", 	/^\s*(\\\s*\\)\s*()$/, 				1, 2],
+			["empty", 		/^(\s*)()$/, 						1, 2],
+			["catchall", 	/^()(.*)$/, 						1, 2],
+		]
+		
+		for (var i in regexes) {
+			var match = regexes[i][1].exec(line);
+			if (match != null) {
+				return {
+					token: regexes[i][0], 
+					keyword: match[regexes[i][2]], 
+					body: match[regexes[i][3]],
+					line: line
+				};
 			}
 		}
+	}
+	
+	var removeEmptyLinesAroundSlidedelimiters = function(tokens) {
+		var without = new Array;
+		for (var i in tokens) {
+			var push = true;
+			if (tokens[i].token == "empty") {
+				push = i == 0 || tokens[parseInt(i)-1].token != "slidemark";
+				push &= i != tokens.length-1 && tokens[parseInt(i)+1].token != "slidemark";
+			}
+			if (push) without.push(tokens[i]);
+		}
+		return without;
+	}
+	
+	var countNumberOf = function(char, str) {
+		return str.match(new RegExp(char, "g")).length;
+	}
+	
+	function escapeTags(str) {
+		var escaped = String(str).replace(/</g, '&lt;').replace(/>/g, '&gt;');
+		return escaped;
+	}
+	
+	function unescapeWhitelist(str) {
+		var whitelist = new Array("strong", "img", "em", "span", "h1", "h2", "h3", "h4", "h5", "h6", "!--");
 		
-		tokens[i].body = unescapeWhitelist(escapeTags(tokens[i].body));
+		for (var i in whitelist) {
+			var white = new RegExp("&lt;(\s*[^&]*"+whitelist[i]+"[^&]*)&gt;", "g");
+			str = str.replace(white, "<$1>");
+		}
 		
-		switch (tokens[i].token) {
-			case "slidemark":
-				slides.push(slide);
-				slide = "";
-				break;
-			case "bullet":
-				if (!inList) slide += "<ul>";
-				slide += "<li class='lvl"+countNumberOf("-",tokens[i].keyword)+"'>"+tokens[i].body+"</li>";
-				inList = true;
-				break;
-			case "heading":
-				var lvl = countNumberOf("#",tokens[i].keyword);
-				var clazz = tokens[i].addClass != undefined ? " class='"+tokens[i].addClass+"'" : "";
-				slide += "<h"+lvl+clazz+">"+tokens[i].body+"</h"+lvl+">";
-				break;
-			case "codemark":
-				slide += "<pre class='prettyprint'>\n";
-				inCode = true;
-				break;
-			case "empty":
-				slide += tokens[i].body+"\n";
-				break;
-			case "catchall":
-				slide += tokens[i].body+"\n";
-				break;
-		}
+		return str;
 	}
 	
-	if (inCode) {
-		slide += "</pre>\n";
-	}
-	if (inList) {
-		slide += "</ul>\n";
+	function endOfShowSlide() {
+		return "<p class='endnote'>End of presentation</p><p class='endtip'>(press <strong>ESC</strong> to exit)</p><p class='endcredit'>Made with Slidifier</p>";
 	}
 	
-	slides.push(slide);
-	
-	slides.push(endOfShowSlide());
-	
-	return slides;
-}
-
-var tokenize = function(line) {
-	var regexes = [
-		["slidemark", 	/^\s*(-\s*-\s*-)\s*()$/, 			1, 2],
-		["bullet", 		/^\s*((-\s*)*-)\s*(([^-]|\S)+)$/, 	1, 3],
-		["heading", 	/^\s*((#\s*)*#)\s*(([^#]|\S)+)$/, 	1, 3],
-		["codemark", 	/^\s*(\\\s*\\)\s*()$/, 				1, 2],
-		["empty", 		/^(\s*)()$/, 						1, 2],
-		["catchall", 	/^()(.*)$/, 						1, 2],
-	]
-	
-	for (var i in regexes) {
-		var match = regexes[i][1].exec(line);
-		if (match != null) {
-			return {
-				token: regexes[i][0], 
-				keyword: match[regexes[i][2]], 
-				body: match[regexes[i][3]],
-				line: line
-			};
-		}
-	}
-}
-
-var removeEmptyLinesAroundSlidedelimiters = function(tokens) {
-	var without = new Array;
-	for (var i in tokens) {
-		var push = true;
-		if (tokens[i].token == "empty") {
-			push = i == 0 || tokens[parseInt(i)-1].token != "slidemark";
-			push &= i != tokens.length-1 && tokens[parseInt(i)+1].token != "slidemark";
-		}
-		if (push) without.push(tokens[i]);
-	}
-	return without;
-}
-
-var countNumberOf = function(char, str) {
-	return str.match(new RegExp(char, "g")).length;
-}
-
-function escapeTags(str) {
-	var escaped = String(str).replace(/</g, '&lt;').replace(/>/g, '&gt;');
-	return escaped;
-}
-
-function unescapeWhitelist(str) {
-	var whitelist = new Array("strong", "img", "em", "span", "h1", "h2", "h3", "h4", "h5", "h6", "!--");
-	
-	for (var i in whitelist) {
-		var white = new RegExp("&lt;(\s*[^&]*"+whitelist[i]+"[^&]*)&gt;", "g");
-		str = str.replace(white, "<$1>");
-	}
-	
-	return str;
-}
-
-function endOfShowSlide() {
-	return "<p class='endnote'>End of presentation</p><p class='endtip'>(press <strong>ESC</strong> to exit)</p><p class='endcredit'>Made with Slidifier</p>";
-}
+	return this;
+})();
--- js/slidifier.js
+++ js/slidifier.js
@@ -1,52 +1,60 @@
-var slides;
-var slideCounter;
-
-var submitHandler = function(event) {
-	event.preventDefault();
+var slidifier = (function() {
+	this.slides;
+	this.slideCounter;
 	
-	var slidesSrc = $('textarea[name=slides_src]').val();
-	slides = processSlideSource(slidesSrc);
-	
-	if (isIE()) {
-		for (var i in slides) {
-			slides[i] = slides[i].replace(/\n/g, "<br/>");
+	var submitHandler = function(event) {
+		event.preventDefault();
+		
+		var slidesSrc = $('textarea[name=slides_src]').val();
+		slides = slideparser.processSlideSource(slidesSrc);
+		
+		if (isIE()) {
+			for (var i in slides) {
+				slides[i] = slides[i].replace(/\n/g, "<br/>");
+			}
 		}
-	}
+		
+		slideCounter = 0;
+		
+		refresh();
+		
+		loadTheme();
+		
+		showSlideShow();
+	};
 	
-	slideCounter = 0;
-	
-	refresh();
-	
-	loadTheme();
-	
-	showSlideShow();
-};
-
-var showSlideShow = function() {
-	$("#slideEdit").fadeOut(10, function() {
-		$("#slide").fadeIn(1000, function() {
-			$("#help").slideDown("slow", function() {
-				setTimeout(function() {
-					$("#help").slideUp("slow");
-				}, 3000);
+	var showSlideShow = function() {
+		$("#slideEdit").fadeOut(10, function() {
+			$("#slide").fadeIn(1000, function() {
+				$("#help").slideDown("slow", function() {
+					setTimeout(function() {
+						$("#help").slideUp("slow");
+					}, 3000);
+				});
 			});
 		});
-	});
-};
-
-var isIE = function() {
-	return navigator.userAgent.indexOf('MSIE') != -1;
-};
+	};
+	
+	var isIE = function() {
+		return navigator.userAgent.indexOf('MSIE') != -1;
+	};
+	
+	this.init = function() {
+		$("#slide").hide();
+		$("#urls").hide();
+		$("#help").hide();
+		
+		$('#srcForm textarea').TextAreaResizer();
+		
+		updateClock();
+		setInterval('updateClock()', 1000*60);
+		
+		$("#srcForm").submit(submitHandler);
+	};
+	
+	return this;
+})();
 
 $(document).ready(function() {
-	$("#slide").hide();
-	$("#urls").hide();
-	$("#help").hide();
-	
-	$('#srcForm textarea').TextAreaResizer();
-	
-	updateClock();
-	setInterval('updateClock()', 1000*60);
-	
-	$("#srcForm").submit(submitHandler);
+	slidifier.init();
 });
