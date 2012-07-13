727d6d8da5b5725275479054708a45f090e5b47a
holgerl <holger.ludvigsen@bekk.no>
2012-07-13 19:53:38 +0200

    Lager nå også selve databasen i create-slidifier.sql

--- sql/create-slidifier.sql
+++ sql/create-slidifier.sql
@@ -1,5 +1,7 @@
-CREATE TABLE slideshows (
+CREATE DATABASE slidifier;
+
+CREATE TABLE slidifier.slideshows (
 	id varchar(500) NOT NULL PRIMARY KEY,
 	admin_key varchar(500) NOT NULL,
 	src longtext
-)
+);
