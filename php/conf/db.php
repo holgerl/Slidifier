0fefb9fed310583759132bf806eb0e55cca5518c
Holger Ludvigsen <ludvigsen@gmail.com>
2015-05-09 22:33:53 +0200

    Making the database work with Heroku ClearDB

--- php/conf/db.php
+++ php/conf/db.php
@@ -1,8 +1,15 @@
 <?php
+	$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"],1);
+
 	$dbConfig = array(
-	    'db_host' => 'localhost',
-	    'db_name' => 'slidifier',
-	    'db_user' => 'root',
-	    'db_password' => ''
+	    'db_host' => $server,
+	    'db_name' => $db,
+	    'db_user' => $username,
+	    'db_password' => $password
 	);
 ?>
